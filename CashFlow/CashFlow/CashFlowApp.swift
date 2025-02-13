5401fcb82bd30d0737d30b9b7e253724666fe1b5
kut7728 <kut7728@hanyang.ac.kr>
2025-02-13 17:18:52 +0900

    [CHORE] .gitignore added

---
+++ CashFlow/CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by nelime on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
